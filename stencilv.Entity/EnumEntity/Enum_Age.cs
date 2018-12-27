d0d1ff721a119371208c85268ab29cde645c38ab
chenguangfei <823232492@qq.com>
2018-12-27 17:33:07 +0800

    1227

---
+++ stencilv.Entity/EnumEntity/Enum_Age.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace stencilv.Entity.EnumEntity
+{
+    /// <summary>
+    /// 性别
+    /// </summary>
+    public enum Enum_Age
+    {
+        男,
+        女
+    }
+}
--- stencilv.Entity/sys/sys_User.cs
+++ stencilv.Entity/sys/sys_User.cs
@@ -1,10 +1,35 @@
-﻿namespace stencilv.Entity.sys
+﻿using stencilv.Entity.EnumEntity;
+
+namespace stencilv.Entity.sys
 {
     /// <summary>
     /// 用户表
     /// </summary>
     public class sys_User : sys_Total
     {
+        /// <summary>
+        /// 姓名
+        /// </summary>
         public string UserName { get; set; }
+        /// <summary>
+        /// 帐号
+        /// </summary>
+        public string UserAccount { get; set; }
+        /// <summary>
+        /// 密码
+        /// </summary>
+        public string UserPassword { get; set; }
+        /// <summary>
+        /// 性别
+        /// </summary>
+        public Enum_Age UserAge { get; set; }
+        /// <summary>
+        /// 年龄
+        /// </summary>
+        public int UserGender { get; set; }
+        /// <summary>
+        /// 手机号
+        /// </summary>
+        public string UserPhone { get; set; }
     }
 }
