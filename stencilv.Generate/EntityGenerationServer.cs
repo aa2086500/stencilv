4545d390ff2082138a7b166482a1e46a09be38b4
chenguangfei <823232492@qq.com>
2019-01-03 17:42:28 +0800

    1-3

---
+++ stencilv.Generate/EntityGenerationServer.cs
@@ -0,0 +1,20 @@
+﻿using stencilv.IGenerate;
+using System.Threading.Tasks;
+
+namespace stencilv.Generate
+{
+    public class EntityGenerationServer : IEntityGenerationServer
+    {
+        /// <summary>
+        /// 生成实体类
+        /// </summary>
+        /// <param name="Name"></param>
+        /// <param name="json"></param>
+        /// <returns></returns>
+        public async Task<string> GetGenerationEntity(string Name, string json)
+        {
+            var r = await GetGenerationEntity("dawdwa","dawd");
+            return r;
+        }
+    }
+}
---
+++ stencilv.IGenerate/IEntityGenerationServer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stencilv.IGenerate
+{
+    /// <summary>
+    /// 实体类操作
+    /// </summary>
+    public interface IEntityGenerationServer
+    {
+        /// <summary>
+        /// 生成实体类
+        /// </summary>
+        /// <param name="Name">实体类名称</param>
+        /// <param name="json">实体类值</param>
+        /// <returns></returns>
+        Task<string> GetGenerationEntity(string Name,string json); 
+    }
+}
